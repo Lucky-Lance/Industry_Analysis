e61b7a95b7128d249ad7b5714e7274f3514882f5
fangtiancheng <1773701277@qq.com>
2022-05-05 01:28:20 +0800

    add graphToJson

--- data-process/main.cpp
+++ data-process/main.cpp
@@ -1,7 +1,7 @@
 #include "parser.h"
 #include "graph.h"
-#include "outputJson.h"
 #include <filesystem>
+#include <json/json.h>
 void divideGraph(){
     string linkName = "./Link.csv";
     string nodeName = "./Node.csv";
@@ -13,18 +13,29 @@ void divideGraph(){
     outputSubGraph(nodes, links, subGraphs);
 }
 void analyseTheBiggest(){
-    string linkName = "./output/00000055-Link-00172977.csv";
-    string nodeName = "./output/00000055-Node-00172977.csv";
+    string linkName = "/media/ftc/DATA/output/00000055-Link-00172977.csv";
+    string nodeName = "/media/ftc/DATA/output/00000055-Node-00172977.csv";
     vector<LinkItemType> links = linkReader(linkName);
     map<Hash, ItemType> nodes  = nodeReader(nodeName);
     Graph graph;
     graph.insert(links, nodes);
     graph.analyseDegree();
 }
+void subGraphToJsonForAnalyse(){
+    string linkName = "/media/ftc/DATA/output/00000014-Link-00000044.csv";
+    string nodeName = "/media/ftc/DATA/output/00000014-Node-00000044.csv";
+    vector<LinkItemType> links = linkReader(linkName);
+    map<Hash, ItemType> nodes  = nodeReader(nodeName);
+    ofstream f; f.open("subgraph14.json", ios::out);
+    f << graphToJson(links, nodes) << endl;
+    f.close();
+}
 int main(){
-    auto outputFolder = filesystem::path("./output/");
+    auto outputFolder = filesystem::path("/media/ftc/DATA/output/");
     if(!filesystem::is_directory(outputFolder)){
+        DEBUG
         if(filesystem::create_directory(outputFolder)){
+            DEBUG
             divideGraph();
         }
         else{
@@ -33,6 +44,7 @@ int main(){
         }
     }
     DEBUG
-    analyseTheBiggest();
+//    analyseTheBiggest();
+    subGraphToJsonForAnalyse();
     return 0;
 }
